8dd8616918461a086b60bc486f769c3346e8053d
weiLaoShi <weilingwei_it@163.com>
2019-09-19 15:09:29 +0800

    yuyue-boss-上传配置

--- src/main/java/com/yuyue/boss/BossApplication.java
+++ src/main/java/com/yuyue/boss/BossApplication.java
@@ -2,6 +2,8 @@ package com.yuyue.boss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,11 +17,11 @@ public class BossApplication {
     public static void main(String[] args) { SpringApplication.run(BossApplication.class, args); }
 
     //上线放开
-//    public class AppApplication extends SpringBootServletInitializer {
+//    public class BossApplication extends SpringBootServletInitializer {
 //
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 //        // 注意这里要指向原先用main方法执行的Application启动类
-//        return builder.sources(AppApplication.class);
+//        return builder.sources(BossApplication.class);
 //    }
 }
