334860f57a71f7d228746dbc06a6c58bcfd08777
lyang <Lyangde163@163.com>
2019-09-20 14:19:08 +0800

    娱悦后台-二维码修改路径3

--- src/main/java/com/yuyue/boss/api/controller/QRCodeController.java
+++ src/main/java/com/yuyue/boss/api/controller/QRCodeController.java
@@ -4,6 +4,7 @@ import com.yuyue.boss.annotation.CurrentUser;
 import com.yuyue.boss.api.domain.SystemUser;
 import com.yuyue.boss.enums.ResponseData;
 import com.yuyue.boss.utils.QRCodeUtil;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,18 +28,20 @@ public class QRCodeController extends BaseController{
         getParameterMap(request);
         log.info("生成二维码------------>>/qrCode/getQRCodeWithLogo");
         String content=request.getParameter("id");
-        String qrCodeName = request.getParameter("siteTitle")+request.getParameter("startTime").split(":")[0]+"点";
+        String qrCodeName = request.getParameter("siteTitle")+request.getParameter("startTime").split(" ")[0];
         int size=Integer.parseInt(request.getParameter("size"));
         String url=System.getProperty("user.dir");
-        log.info("---------------------"+url);
-        System.out.println(url);
-        String logoPath=System.getProperty("user.dir")+"/src/main/resources/logo/logo.png";
-        System.out.println(content);
-        System.out.println(qrCodeName);
-        System.out.println(size);
-        System.out.println(logoPath);
+
+        log.info("system---------------------"+url);
+        //logo本地路径
+        //String logoLocalPath=System.getProperty("user.dir")+"/src/main/resources/logo/logo.png";
+        //服务器路径
+        String path = ClassUtils.getDefaultClassLoader().getResource("").getPath();
+        log.info(path);
+        String logoPath = path+"static/logo/logo.png";
+        log.info(logoPath);
         String s = QRCodeUtil.zxingCodeCreate(content, qrCodeName, size, logoPath);
-        System.out.println("二维码路径："+s);
+        log.info("二维码路径："+s);
         return new ResponseData(s);
     }
 
