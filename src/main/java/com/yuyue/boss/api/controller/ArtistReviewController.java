6c7bffa7f276987767db84498e65d340317dfdc3
lyang <Lyangde163@163.com>
2019-10-21 14:32:17 +0800

    娱悦后台- 广告投放，艺人

--- src/main/java/com/yuyue/boss/api/controller/ArtistReviewController.java
+++ src/main/java/com/yuyue/boss/api/controller/ArtistReviewController.java
@@ -5,13 +5,18 @@ import com.github.pagehelper.PageInfo;
 import com.yuyue.boss.annotation.LoginRequired;
 import com.yuyue.boss.api.domain.AppUser;
 import com.yuyue.boss.api.domain.ArtistReview;
+import com.yuyue.boss.api.domain.UploadFile;
 import com.yuyue.boss.api.service.AppUserService;
 import com.yuyue.boss.api.service.ArtistReviewService;
+import com.yuyue.boss.api.service.VideoService;
 import com.yuyue.boss.enums.CodeEnum;
 import com.yuyue.boss.enums.ResponseData;
+import com.yuyue.boss.utils.GetImageFromVideoUtil;
+import com.yuyue.boss.utils.ResultJSONUtils;
 import com.yuyue.boss.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.bytedeco.javacv.FrameGrabber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/artistReview" ,produces = "application/json; charset=UTF-8")
@@ -32,6 +38,8 @@ public class ArtistReviewController extends BaseController {
     private AppUserService appUserService;
     @Autowired
     private SendController sendController;
+    @Autowired
+    private VideoService videoService;
 
     /**
      * 获取艺人申请
@@ -96,14 +104,37 @@ public class ArtistReviewController extends BaseController {
                     return new ResponseData("该用户不存在");
                 if ("1".equals(appUserMsg.getUserType())  || StringUtils.isEmpty(appUserMsg.getUserType()))
                     appUserMsg.setUserType("2");
-                if ("3".equals(appUserMsg.getUserType())  || StringUtils.isEmpty(appUserMsg.getUserType()))
+                if ("3".equals(appUserMsg.getUserType()) )
                     appUserMsg.setUserType("4");
                 appUserMsg.setFrontCover("http://101.37.252.177:82/defaultFrontCover/defaultFrontCover.png");
                 System.out.println(appUserMsg);
                 appUserService.updateAppUser(appUserMsg);
+                if (StringUtils.isNotEmpty(artistReviewList.get(0).getCategoryId()) &&
+                        StringUtils.isNotEmpty(artistReviewList.get(0).getVideoAddress())){
+                    System.out.println(artistReviewList.get(0));
+                    UploadFile uploadFile = new UploadFile();
+                    uploadFile.setId(UUID.randomUUID().toString().replace("-","").toUpperCase());
+                    uploadFile.setAuthorId(appUserMsg.getId());
+                    uploadFile.setCategoryId(artistReviewList.get(0).getCategoryId());
+                    uploadFile.setTitle(artistReviewList.get(0).getDescription());
+                    uploadFile.setFilesName(artistReviewList.get(0).getDescription());
+                    uploadFile.setFilesPath(artistReviewList.get(0).getVideoAddress());
+                    uploadFile.setDescription(artistReviewList.get(0).getDescription());
+                    String image = null;
+                    try {
+                        image = GetImageFromVideoUtil.getImage();
+                    } catch (FrameGrabber.Exception e) {
+                        e.printStackTrace();
+                        return new ResponseData("生成视频第一帧图片失败");
+                    }
+                    uploadFile.setVideoAddress(image);
+                    uploadFile.setTableName(ResultJSONUtils.getHashValue("yuyue_upload_file_",appUserMsg.getId()));
+                    System.out.println(uploadFile);
+                    videoService.insertVideo(uploadFile);
+                }
+
             }
             sendController.sendShowJPush(artistReviewList.get(0),appUserMsg,status);
-
             artistReviewService.updateArtistReviewStatus(id,status);
         }else {
             return new ResponseData(CodeEnum.PARAM_ERROR.getCode(),"参数：status不合法！！");
