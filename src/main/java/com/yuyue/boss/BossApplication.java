9e330e300be4d3adc80bb038e2c1a419d5b7b55f
weiLaoShi <weilingwei_it@163.com>
2019-11-25 14:57:47 +0800

    yuyue-boss-全局null处理

--- src/main/java/com/yuyue/boss/BossApplication.java
+++ src/main/java/com/yuyue/boss/BossApplication.java
@@ -1,15 +1,25 @@
 package com.yuyue.boss;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.ErrorPageFilter;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 //开启定时任务
 @EnableScheduling
@@ -29,6 +39,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         return builder.sources(BossApplication.class);
     }
 
+    //Page报错处理
     @Bean
     public ErrorPageFilter errorPageFilter() {
         return new ErrorPageFilter();
@@ -40,5 +51,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         filterRegistrationBean.setFilter(filter);
         filterRegistrationBean.setEnabled(false);
         return filterRegistrationBean;
+    }
+
+    // 为null处理
+    @Bean
+    public HttpMessageConverters fastJsonConverters() {
+        FastJsonHttpMessageConverter4 fastConverter = new FastJsonHttpMessageConverter4();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.IgnoreNonFieldGetter,
+                SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        List supportedMediaTypes = new ArrayList();
+        supportedMediaTypes.add(new MediaType("text", "json", Charset.forName("utf8")));
+        supportedMediaTypes.add(new MediaType("application", "json", Charset.forName("utf8")));
+        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
     }
 }
